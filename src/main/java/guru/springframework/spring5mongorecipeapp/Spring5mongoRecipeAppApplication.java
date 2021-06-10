ae7d5b160c8ef88e872c1959ce2e7a25db47577b
Kumar Sundararam <kumarisaac@gmail.com>
2021-06-10 21:38:47 +0530

    Initial commit

---
+++ src/main/java/guru/springframework/spring5mongorecipeapp/Spring5mongoRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5mongorecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5mongoRecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5mongoRecipeAppApplication.class, args);
+    }
+
+}
